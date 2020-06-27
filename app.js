510c7866e6482424d223a17d787e24374c3c303d
simeon4real <simeon.udoh45@gmail.com>
2020-06-27 11:26:21 +0100

    fixed :zap: overflow of screen on mobile devices

--- app.js
+++ app.js
@@ -1,59 +1,48 @@
-const button = document.querySelectorAll(".calc__button");
-const screen = document.querySelector(".screen");
-const equal = document.querySelector(".equal");
-const clear = document.querySelector(".clear");
-const undo = document.querySelector(".undo");
+const buttons = document.querySelectorAll('.calc__button');
+const screen = document.querySelector('.screen');
+const equal = document.querySelector('.equal');
+const clear = document.querySelector('.clear');
+const undo = document.querySelector('.undo');
 
-let display = "";
+let display = '';
 
-button.forEach(button => {
-	button.addEventListener("click", () => {
-		//logic for button
-		buttonValue = button.getAttribute("data-num");
-		getDisplay();
-	});
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    //logic for button
+    buttonValue = button.getAttribute('data-num');
+    getDisplay();
+  });
 });
 
-
 function getDisplay() {
-	display += buttonValue;
-	screen.textContent = display;
-
-//todo: add screen character limit of 14
+  display += buttonValue;
+  screen.textContent = display;
 }
 
 function getResult() {
-	screen.textContent = eval(screen.textContent);
+  screen.textContent = eval(screen.textContent);
 }
-
 // Clear Screen
 function clearScreen() {
-	screen.textContent = "0";
-	display = "";
+  screen.textContent = '0';
+  display = '';
 }
 
 /* Implements the delete button. */
 
 function undoInput() {
-	display = display.slice(0, -1);
-	screen.textContent = display; 
+  display = display.slice(0, -1);
+  screen.textContent = display;
 
-	if (display.length < 1) {
-		clearScreen(); 
-	}
-
-	
+  if (display.length < 1) clearScreen();
 }
 
-
-screen.addEventListener("click", getDisplay);
-clear.addEventListener("click", clearScreen);
-undo.addEventListener("click", undoInput);
-equal.addEventListener("click", getResult);
+screen.addEventListener('click', getDisplay);
+clear.addEventListener('click', clearScreen);
+undo.addEventListener('click', undoInput);
+equal.addEventListener('click', getResult);
 
 // Footer date - Updated automatically
 
 const date = new Date().getFullYear();
-document.getElementById("year").innerHTML = date;
-
-
+document.getElementById('year').innerHTML = date;
